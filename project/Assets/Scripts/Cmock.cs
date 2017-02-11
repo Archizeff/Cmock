3bd86c91d5228b69b4e254c4e134512acf2009fd
mihail-ПК\mihail <althunin@gmail.com>
2017-02-11 18:14:41 +0300

    Experements with Physics.

---
+++ project/Assets/Scripts/Cmock.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cmock : MonoBehaviour {
+
+    public float minSpeed, maxSpeed;
+
+    Vector2 impulse;
+    Rigidbody2D body;
+
+    public Vector2 Impulse{
+        set {
+            if (value.magnitude < minSpeed)
+            {
+                impulse = value / value.magnitude * minSpeed;
+                return;
+            }
+            if (value.magnitude > maxSpeed)
+            {
+                impulse = value / value.magnitude * maxSpeed;
+                return;
+            }
+        }
+    }
+
+    void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+        impulse = Vector2.up * minSpeed;
+    }
+
+    void FixedUpdate()
+    {
+        if (body.velocity != impulse)
+        {
+            body.AddForce((impulse - body.velocity) * 100);
+        }
+    }
+
+}
